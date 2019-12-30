c76425511cd63ee313d88d8353e4ec5a27c97bb4
sclarke5 <sclarke5555@gmail.com>
2019-12-30 15:59:32 -0500

    first commit

---
+++ client/reducers/posts.js
@@ -0,0 +1,22 @@
+// a reducer takes two things:
+
+// 1. the action (info about what happened)
+// 2. a copy of current state 
+
+function posts(state = [], action){
+  switch(action.type){
+    case 'INCREMENT_LIKES' : 
+    const i = action.index;
+      return [
+        ...state.slice(0,i), // before the one we're updating
+        {...state[i], likes: state[i].likes + 1}, 
+        ...state.slice(i + 1), // after the one updating
+      ]
+    // return the updated state
+    default:
+      return state;
+  }
+  return state;
+}
+
+export default posts;
